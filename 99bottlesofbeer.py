ea4beaf94c3ebae16cc7688d1679042411aef368
Bruce Warren <34455271+brucemwarren@users.noreply.github.com>
2019-07-13 21:13:20 -0400

    99 Bottles of Beer Lyrics Generator

---
+++ 99bottlesofbeer.py
@@ -0,0 +1,22 @@
+# Program to print out the lyrics to "99 Bottles of Beer (On The Wall)"
+#
+# The Rules:
+#
+# If you are going to use a list for all of the numbers, do not manually type them all in. Instead, use a built in
+# function.
+# Besides the phrase "take one down," you may not type in any numbers/names of numbers directly into your song lyrics.
+# Remember, when you reach 1 bottle left, the word "bottles" becomes singular.
+# Put a blank line between each verse of the song.
+# Project from: https://www.reddit.com/r/beginnerprojects/comments/19kxre/project_99_bottles_of_beer_on_the_wall_lyrics/
+
+bottles = 99
+
+while bottles > 1:  # While the count is greater than 1, it will print the lyric, plus a blank line.
+    print(bottles, "bottles of beer on the wall!", bottles, "bottles of beeeeer! Take one down and pass it around",
+          (bottles - 1), "bottles of beer on the wall!!!!")
+    print()
+    bottles = bottles - 1  # Adjusts the bottle count
+else:
+    # Adjusts the lyrics for the last bottle.
+    print(bottles, "bottle of beer on the wall!", bottles,
+          "bottle of beeeeer! Take it down and pass it around, no more bottles of beer on the wall!!!!")
